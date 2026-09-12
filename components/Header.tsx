88e22201799b9e9cd11b96e30444b0e6094a4be8
Dwyane Saludo Sido <disowned.ay@gmail.com>
2026-09-12 14:20:08 +0800

    Initial Commit

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <header>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/works">Works</Link>
+      </nav>
+    </header>
+  );
+}
